9d9b0ebf5df672ca80e8e4c5ea57377bd145a606
nik-boop <71924325+nik-boop@users.noreply.github.com>
2021-04-23 20:19:30 +0300

    Update List R.py

--- List R.py
+++ List R.py
@@ -1,31 +1,34 @@
+
 class ring:
-    def __init__(self,val):
-        self.__val = val
+    def __init__(self, val):
+        self.val = val
         self.next = None
 
     def __str__(self):
-        return f'{self.__val}'
+        return f'{self.val}'
 
     def data(self):
-        return self.__val
+        return self.val
 
-    def Next(self,next):
+    def Next(self, next):
         self.next = next
         return self.next
-    def Head(self,next):
+
+    def Head(self, next):
         self.next = next
         return self
 
+
 class listr:
-    def __init__(self,val=None):
-        if val != None:
+    def __init__(self, val=None):
+        if val is not None:
             try:
                 iterator = iter(val)
             except TypeError:
                 self.head = ring(val)
                 self.last = self.head
             else:
-                List = list(val)
+                #List = list(val)
                 self.head = ring(val[0])
                 self.last = self.head
                 for i in val[1:]:
@@ -34,8 +37,10 @@ class listr:
             self.head = None
             self.last = None
 
+    def __len__(self):
+        return self.len()
 
-    def append(self,val):
+    def append(self, val):
         if self.head == None:
             self.head = ring(val)
             self.last = self.head
@@ -50,18 +55,81 @@ class listr:
             a = a.next
         else:
             yield a.data()
+
     def Head(self,var):
         if self.head == None:
             self.head = ring(var)
             self.last = self.head
         else:
             self.head = ring(var).Head(self.head)
+    def len(self):
+        if self.head == None: return 0
+        else: k=0
+        a = self.head
+        while a.next != None:
+            a = a.next
+            k+=1
+        else:
+            k+=1
+        return k
 
+    def is_empty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+    def find(self,number = None, value = None):
+        if number != None:
+            if number > self.len()-1 or number < 0 : return 'IndexError'
+            a = self.head
+            for i in range(number):
+                a = a.next
+
+            return a
+
+    def rewrites(self,number,value):
+        self.find(number).val = value
+
+    def pop(self,number = None):
+        fl = False
+        if number is None: number = self.len() - 1; fl = True
+        if self.head is None: return None
+        if number > self.len()-1 or number < 0 : return 'IndexError'
+        if number == 0:
+            var = self.head.data()
+
+            if self.head.next is not None:
+                if self.len() != 2:
+                    self.head = self.head.next
+                else:
+                    self.head = self.head.next
+                    self.last = self.head
+            else:
+                self.head=None
+                self.last=None
+            return var
+        a = self.head
+        for i in range(number-1):
+            a = a.next
+        else:
+            var = a.next.data()
+            if fl != True:
+                a.Next(a.next.next)
+            else:
+                a.Next(None)
+            return var
+
+#Тест
 l = listr([-5, -4, -3, -2, -1, 0, 1])
 l.append(2)
 for i in range(3,10):
     l.append(i)
 print(l.head.data(),l.last.data())
-print(list(l.show()))
+
 l.Head(-6)
+l1 = listr()
 print(list(l.show()))
+print(len(l))
+print(l1.pop())
+print(list(l.show()))
+print(l.find(len(l)-1).data())
