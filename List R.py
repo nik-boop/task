06d6f20aff5bc8c7378d234c40d9f29183a0750c
nik-boop <71924325+nik-boop@users.noreply.github.com>
2021-04-23 17:44:41 +0300

    Add files via upload

---
+++ List R.py
@@ -0,0 +1,67 @@
+class ring:
+    def __init__(self,val):
+        self.__val = val
+        self.next = None
+
+    def __str__(self):
+        return f'{self.__val}'
+
+    def data(self):
+        return self.__val
+
+    def Next(self,next):
+        self.next = next
+        return self.next
+    def Head(self,next):
+        self.next = next
+        return self
+
+class listr:
+    def __init__(self,val=None):
+        if val != None:
+            try:
+                iterator = iter(val)
+            except TypeError:
+                self.head = ring(val)
+                self.last = self.head
+            else:
+                List = list(val)
+                self.head = ring(val[0])
+                self.last = self.head
+                for i in val[1:]:
+                    self.append(i)
+        else:
+            self.head = None
+            self.last = None
+
+
+    def append(self,val):
+        if self.head == None:
+            self.head = ring(val)
+            self.last = self.head
+        else:
+            self.last = self.last.Next(ring(val))
+
+    def show(self):
+        if self.head == None: return None
+        a = self.head
+        while a.next != None:
+            yield a.data()
+            a = a.next
+        else:
+            yield a.data()
+    def Head(self,var):
+        if self.head == None:
+            self.head = ring(var)
+            self.last = self.head
+        else:
+            self.head = ring(var).Head(self.head)
+
+l = listr([-5, -4, -3, -2, -1, 0, 1])
+l.append(2)
+for i in range(3,10):
+    l.append(i)
+print(l.head.data(),l.last.data())
+print(list(l.show()))
+l.Head(-6)
+print(list(l.show()))
